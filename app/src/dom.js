04f4595fcac40d606f66618851dc2c74d357628c
przemyslaw-koz <przemyslaw.koz@gmail.com>
2026-02-01 10:29:02 +0100

    exporting getdom

--- app/src/dom.js
+++ app/src/dom.js
@@ -1,6 +1,4 @@
-//dom.js
-
-const getDom = () => {
+export const getDom = () => {
   const dom = {
     flashEl: document.getElementById("flash"),
     treasureEl: document.getElementById("treasure"),
@@ -8,14 +6,16 @@ const getDom = () => {
     livesEl: document.getElementById("lives"),
     enemyEl: document.getElementById("enemy"),
     hudEl: document.getElementById("hud"),
-    container: document.getElementById("container"),
-    square: document.getElementById("square"),
-    startButton: document.getElementById("start-button"),
-    bgm: document.getElementById("bgm"),
+    containerEl: document.getElementById("container"),
+    squareEl: document.getElementById("square"),
+    startBtnn: document.getElementById("start-button"),
+    bgmEl: document.getElementById("bgm"),
     overlayEl: document.getElementById("overlay"),
     overlayTitleEl: document.getElementById("overlay-title"),
     overlayTextEl: document.getElementById("overlay-text"),
     restartBtn: document.getElementById("restart-btn"),
     muteBtn: document.getElementById("mute-btn"),
   };
+
+  return dom;
 };
