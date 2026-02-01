5497446240bdea1ad423f4930953d7f98c34fa09
przemyslaw-koz <przemyslaw.koz@gmail.com>
2026-02-01 10:25:40 +0100

    fixed getDom

--- app/src/dom.js
+++ app/src/dom.js
@@ -1,21 +1,21 @@
 //dom.js
 
-cost getDom = () =>{
+const getDom = () => {
   const dom = {
-    flashEl: document.getElementById("flash");
-    treasureEl: document.getElementById("treasure");
-    scoreEl: document.getElementById("score");
-    livesEl: document.getElementById("lives");
-    enemyEl: document.getElementById("enemy");
-    hudEl: document.getElementById("hud");
-    container: document.getElementById("container");
-    square: document.getElementById("square");
-    startButton: document.getElementById("start-button");
-    bgm: document.getElementById("bgm");
-    overlayEl: document.getElementById("overlay");
-    overlayTitleEl: document.getElementById("overlay-title");
-    overlayTextEl: document.getElementById("overlay-text");
-    restartBtn: document.getElementById("restart-btn");
-    muteBtn: document.getElementById("mute-btn");
-  } 
-}
+    flashEl: document.getElementById("flash"),
+    treasureEl: document.getElementById("treasure"),
+    scoreEl: document.getElementById("score"),
+    livesEl: document.getElementById("lives"),
+    enemyEl: document.getElementById("enemy"),
+    hudEl: document.getElementById("hud"),
+    container: document.getElementById("container"),
+    square: document.getElementById("square"),
+    startButton: document.getElementById("start-button"),
+    bgm: document.getElementById("bgm"),
+    overlayEl: document.getElementById("overlay"),
+    overlayTitleEl: document.getElementById("overlay-title"),
+    overlayTextEl: document.getElementById("overlay-text"),
+    restartBtn: document.getElementById("restart-btn"),
+    muteBtn: document.getElementById("mute-btn"),
+  };
+};
